767309b36247e3e0c14d64a665becbb8b865d9df
tanuj8124 <tanujkashyap8124@gmail.com>
2025-09-02 00:31:30 +0530

    d

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Page() {
               <CardTitle className="flex items-center justify-between">
                 <span>Towards Sachivalaya/GIFT</span>
                 {nextToGift[0] ? (
-                  <span className="text-sm font-medium text-teal-700">Next Metro at<span className="whitespace-nowrap">{formatHMPM(nextToGift[0])}</span></span>
+                  <span className="text-sm font-medium text-teal-700">Next Metro at <span className="whitespace-nowrap">{formatHMPM(nextToGift[0])}</span></span>
                 ) : (
                   <span className="text-sm text-red-600">No times</span>
                 )}
